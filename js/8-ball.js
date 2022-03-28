ffaee57f0fc16c9543af8f2b3902ff9309d4b5c7
MilesAngels <mangels@alphaworks.tech>
2022-03-28 13:53:17 -0700

    change in js

--- js/8-ball.js
+++ js/8-ball.js
@@ -1,8 +1,32 @@
-let eight_ball_img = document.getElementsByClassName("magic-8-ball-img");
-let submit = document.getElementById("submit_button");
+let eight_ball_img = document.getElementsByClassName("outer-sphere");
+let submit = document.getElementById("answer");
 let question = document.getElementById("user_question");
-let resultsArray = [];
-function random(){
-    let randomNumber = Math.floor(Math.random() * 20);
+
+let answersArray = [];
+for (let i = 1; i <= 20; i++){
+    let source = `./img/magic8ball_${i}.png`;
+    answersArray.push(source);
+}
+
+//returns a number with min inclusive and max inclusive
+function random(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;;
     return randomNumber;
 }
+
+console.log(random(1, 20));
+
+function addElements () {
+    let sphere = document.createElement("div");
+    let newImage = document.createElement("div");
+    let form = document.getElementById("form");
+    form.appendChild(sphere);
+    sphere.setAttribute("class", "outer-sphere");
+    sphere.appendChild(newImage);
+    newImage.setAttribute("id", "magic-8-ball")
+    newImage.style.backgroundImage = "url('../img/magic8ball_start.png')";
+}
+
+addElements();
