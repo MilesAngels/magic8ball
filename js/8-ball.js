277cd9e85c9fd0408d52be31b503a83d06e849cb
MilesAngels <mangels@alphaworks.tech>
2022-03-28 10:53:51 -0700

    change in js

---
+++ js/8-ball.js
@@ -0,0 +1,7 @@
+let eight_ball_img = document.getElementsByClassName("magic-8-ball-img");
+let submit = document.getElementById("submit_button");
+let question = document.getElementById("user_question");
+
+function random(){
+    
+}
